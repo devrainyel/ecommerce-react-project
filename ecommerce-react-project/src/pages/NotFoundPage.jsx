d57acf67d0a47052a4b91b671904475d13272b1c
Rainyel <devrainyel@gmail.com>
2025-10-03 00:25:32 +0800

    Solve 7o pass the cart into the Header in Not Found Page

--- ecommerce-react-project/src/pages/NotFoundPage.jsx
+++ ecommerce-react-project/src/pages/NotFoundPage.jsx
@@ -1,11 +1,11 @@
 import { Header } from '../components/Header';
 import './NotFoundPage.css';
 
-export function NotFoundPage() {
+export function NotFoundPage( {cart} ) {
     return (
         <>
             <title>404 Page Not Found</title>
-            <Header />
+            <Header cart={cart} />
             <div className="not-found-container">
                 <div className="not-found-number">404</div>
                 <div className="not-found-message">Page Not Found</div>
