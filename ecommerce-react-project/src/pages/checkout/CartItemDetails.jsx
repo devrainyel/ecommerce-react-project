654e8c59c45ab13d3cf9e1b2c1431b9bcd6261f5
Rainyel <devrainyel@gmail.com>
2025-10-04 10:51:29 +0800

    Solve 8g exercise create controlled input for the quantity textbox

--- ecommerce-react-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-react-project/src/pages/checkout/CartItemDetails.jsx
@@ -3,45 +3,59 @@ import axios from 'axios';
 import './CartItemDetails.css';
 import { useState } from 'react';
 
-export function CartItemDetails( { cartItem, loadCart } ) {
+export function CartItemDetails({ cartItem, loadCart }) {
+  const [isQuantityUpdating, setIsQuantityUpdating] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+
   const deleteCartItem = async () => {
     await axios.delete(`api/cart-items/${cartItem.productId}`);
     await loadCart();
-  }
-
-  const [isQuantityUpdating, setIsQuantityUpdating] = useState(false);
+  };
 
   const updateQuantity = async () => {
-    if(isQuantityUpdating) {
-      setIsQuantityUpdating(false)
+    if (isQuantityUpdating) {
+      setIsQuantityUpdating(false);
     } else {
-      setIsQuantityUpdating(true)
+      setIsQuantityUpdating(true);
     }
-  }
+  };
 
   return (
     <>
-        <img className="product-image" src={cartItem.product.image} />
+      <img className='product-image' src={cartItem.product.image} />
 
-        <div className="cart-item-details">
-        <div className="product-name">{cartItem.product.name}</div>
-        <div className="product-price">
-            {formatMoney(cartItem.product.priceCents)}
+      <div className='cart-item-details'>
+        <div className='product-name'>{cartItem.product.name}</div>
+        <div className='product-price'>
+          {formatMoney(cartItem.product.priceCents)}
         </div>
-        <div className="product-quantity">
-            <span>
-            Quantity: {isQuantityUpdating ? <input type="text" className="quantity-input" /> : <span className="quantity-label">{cartItem.quantity}</span>}  
-            </span>
-            <span 
-              className="update-quantity-link link-primary"
-              onClick={updateQuantity}
-            >Update</span>
-            <span 
-              className="delete-quantity-link link-primary"
-              onClick={deleteCartItem}
-            >Delete</span>
+        <div className='product-quantity'>
+          <span>
+            Quantity:
+            {isQuantityUpdating ? (
+              <input type='text' className='quantity-input' value={quantity} onChange={(event) => {
+              const inputValue =  event.target.value;
+              setQuantity(inputValue);
+              console.log(inputValue);
+              }} />
+            ) : (
+              <span className='quantity-label'>{cartItem.quantity}</span>
+            )}
+          </span>
+          <span
+            className='update-quantity-link link-primary'
+            onClick={updateQuantity}
+          >
+            Update
+          </span>
+          <span
+            className='delete-quantity-link link-primary'
+            onClick={deleteCartItem}
+          >
+            Delete
+          </span>
         </div>
-        </div>
+      </div>
     </>
   );
 }
