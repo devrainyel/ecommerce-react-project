6078df9f0b23c4bd7d5b83ce7ebd1ace2831a66f
Rainyel <devrainyel@gmail.com>
2025-10-04 10:40:47 +0800

    Solve 8f exercise make the input show feature upon clicking Update

--- ecommerce-react-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-react-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,11 +1,22 @@
 import { formatMoney } from '../../utils/money';
 import axios from 'axios';
 import './CartItemDetails.css';
+import { useState } from 'react';
 
 export function CartItemDetails( { cartItem, loadCart } ) {
   const deleteCartItem = async () => {
     await axios.delete(`api/cart-items/${cartItem.productId}`);
     await loadCart();
+  }
+
+  const [isQuantityUpdating, setIsQuantityUpdating] = useState(false);
+
+  const updateQuantity = async () => {
+    if(isQuantityUpdating) {
+      setIsQuantityUpdating(false)
+    } else {
+      setIsQuantityUpdating(true)
+    }
   }
 
   return (
@@ -19,10 +30,12 @@ export function CartItemDetails( { cartItem, loadCart } ) {
         </div>
         <div className="product-quantity">
             <span>
-            Quantity: <input type="text" className="quantity-input" /> 
-            <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity: {isQuantityUpdating ? <input type="text" className="quantity-input" /> : <span className="quantity-label">{cartItem.quantity}</span>}  
             </span>
-            <span className="update-quantity-link link-primary">Update</span>
+            <span 
+              className="update-quantity-link link-primary"
+              onClick={updateQuantity}
+            >Update</span>
             <span 
               className="delete-quantity-link link-primary"
               onClick={deleteCartItem}
