d15ea56b1668d1d1894695ee3b5263a3924469c7
Rainyel <devrainyel@gmail.com>
2025-10-04 11:44:48 +0800

    Solve 8j exercise create controlled input for the search bar

--- ecommerce-react-project/src/components/Header.jsx
+++ ecommerce-react-project/src/components/Header.jsx
@@ -3,14 +3,25 @@ import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 import CartIcon from '../assets/images/icons/cart-icon.png';
 import SearchIcon from '../assets/images/icons/search-icon.png';
+import { useState } from 'react';
 import './Header.css';
 
 export function Header( { cart }) {
+    const [search, setSearch] = useState('');
+
     let totalQuantity = 0;
 
     cart.forEach ((cartItem) => {
         totalQuantity += cartItem.quantity;
     });
+
+    function updateSearchValue(event) {
+        setSearch(event.target.value);
+    }
+
+    function searchProduct() {
+        console.log(search);
+    }
 
     return (
         <>
@@ -25,9 +36,9 @@ export function Header( { cart }) {
                 </div>
 
                 <div className="middle-section">
-                    <input className="search-bar" type="text" placeholder="Search" />
+                    <input className="search-bar" type="text" placeholder="Search" onClick={updateSearchValue} />
 
-                    <button className="search-button">
+                    <button className="search-button" onClick={searchProduct}>
                     <img className="search-icon" src={SearchIcon} />
                     </button>
                 </div>
